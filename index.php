6eed9b8d4f570c832f6002912059636f3cb886ca
pinku7499 <pinku7499@gmail.com>
2018-03-19 22:25:36 +0530

    modified wit better functions

--- index.php
+++ index.php
@@ -1,48 +1,75 @@
 <?php
-$word = $_POST['word'];
-$source= file_get_contents('http://www.dictionary.com/browse/'.$word.'?s=t');
 
-$start=strpos($source,'<div class="def-content">');
-$array = array();
-$count =0;
-while($start && $count<5){
-  $source=substr($source,$start);
-  $end=strpos($source,'</div>');
-  $array[]=substr($source,0,$end);
-  $source=substr($source,$end);
-  $start=strpos($source,'<div class="def-content">');
-  $count+=1;
-}
+  function mkurl($word){
+    $v="http://www.dictionary.com/browse/".$word."?s=t";
+    return $v;
+  }
 
-?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <form class="input" action="index.php" method="POST">
-      <label for="word"><span><h1>Search:</h1><span></label>
-      <input type="text" name="word" value=""/placeholder="Enter Your Word">
-      <input type="submit" name="submit" value="Submit"/>
-    </form>
-    <br/>
-    <div class="background">
-      <?php
-      echo '<h1>'.$word.' --</h1>';
-      if(!empty($array)){
-        foreach($array as $val){
-        $val=strip_tags($val);
-         echo '<p>--'.$val."</p>";
-        }
-      }
-      else{
-        echo 'Not a Word to Define';
-      }
-       ?>
-    </div>
-  </body>
-</html>
+
+
+  function curlget($url){
+    $curl=curl_init();
+     curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+     curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+     curl_setopt($curl, CURLOPT_URL, $url);
+     $result=curl_exec($curl);
+     curl_close($curl);
+     return $result;
+  }
+
+  $definition=array();
+
+  function pathobject($param){
+    $doc_object=new DOMDocument();
+    @$doc_object->loadHTML($param);
+    $xmlpath=new DOMXPath($doc_object);
+    return $xmlpath;
+  }
+
+  $link=mkurl($_POST['word']);
+
+
+  $page=curlget($link);
+
+  $pagepath=pathobject($page);
+
+  $defi = $pagepath->query('//div[@class="def-content"]');
+
+  if($defi->length > 0){
+    for($i=0;$i<$defi->length;$i++){
+      $definition['def'][]=$defi->item($i)->nodeValue;
+    }
+  }
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Dictionary</title>
+     <link rel="stylesheet" href="style.css">
+   </head>
+   <body>
+     <form class="" action="index.php" method="post">
+       <label for="word">Search:</label>
+       <input type="text" name="word" value="" placeholder="Enter Word"/>
+       <button type="submit" name="button">Submit</button>
+     </form>
+     <br/>
+       <div class="container">
+       <?php
+       if(!empty($definition)){
+           foreach($definition['def'] as $val){
+         //  $val=strip_tags($val);
+            echo '<p>--'.$val."</p>";
+           }
+         }
+         else{
+           echo 'Not a Word to Define';
+         }
+
+        ?>
+      </div>
+   </body>
+ </html>
